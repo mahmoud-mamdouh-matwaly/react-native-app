c8f8d3c9bac3dff31761fd13132834c55a32bae9
Mahmoud Mamdouh <mahmoud.m.alawady@gmail.com>
2019-06-13 12:52:55 +0200

    test commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Routes from "./src/components/Routes";
+// import LoginScreen from "./src/components/LoginScreen";
+
+export default class App extends React.Component {
+  render() {
+    return <Routes />;
+  }
+}
